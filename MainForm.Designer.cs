84a7fd1e9c3acbeb1ccbde5a137db2af57b01689
christiankencana <christiankencana28@gmail.com>
2025-11-17 16:45:38 +0700

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace test_basic_dotnet_forms
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.DateTimePicker dtpBirthDate;
+        private System.Windows.Forms.CheckBox chkSubscribe;
+        private System.Windows.Forms.RadioButton rbStudent;
+        private System.Windows.Forms.RadioButton rbProfessional;
+        private System.Windows.Forms.ListBox lstItems;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnShowDialog;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.GroupBox grpStatus;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            this.dtpBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.chkSubscribe = new System.Windows.Forms.CheckBox();
+            this.rbStudent = new System.Windows.Forms.RadioButton();
+            this.rbProfessional = new System.Windows.Forms.RadioButton();
+            this.lstItems = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnShowDialog = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.grpStatus = new System.Windows.Forms.GroupBox();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.grpStatus.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(900, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "&New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(20, 40);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(100, 37);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(20, 70);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(100, 67);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(200, 20);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(20, 100);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(45, 13);
+            this.lblGender.TabIndex = 5;
+            this.lblGender.Text = "Gender:";
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Location = new System.Drawing.Point(100, 97);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(200, 21);
+            this.cmbGender.TabIndex = 6;
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.Location = new System.Drawing.Point(20, 130);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(57, 13);
+            this.lblBirthDate.TabIndex = 7;
+            this.lblBirthDate.Text = "Birth Date:";
+            // 
+            // dtpBirthDate
+            // 
+            this.dtpBirthDate.Location = new System.Drawing.Point(100, 127);
+            this.dtpBirthDate.Name = "dtpBirthDate";
+            this.dtpBirthDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpBirthDate.TabIndex = 8;
+            // 
+            // chkSubscribe
+            // 
+            this.chkSubscribe.AutoSize = true;
+            this.chkSubscribe.Location = new System.Drawing.Point(100, 160);
+            this.chkSubscribe.Name = "chkSubscribe";
+            this.chkSubscribe.Size = new System.Drawing.Size(128, 17);
+            this.chkSubscribe.TabIndex = 9;
+            this.chkSubscribe.Text = "Subscribe Newsletter";
+            this.chkSubscribe.UseVisualStyleBackColor = true;
+            // 
+            // grpStatus
+            // 
+            this.grpStatus.Controls.Add(this.rbProfessional);
+            this.grpStatus.Controls.Add(this.rbStudent);
+            this.grpStatus.Location = new System.Drawing.Point(20, 190);
+            this.grpStatus.Name = "grpStatus";
+            this.grpStatus.Size = new System.Drawing.Size(280, 70);
+            this.grpStatus.TabIndex = 10;
+            this.grpStatus.TabStop = false;
+            this.grpStatus.Text = "Status";
+            // 
+            // rbStudent
+            // 
+            this.rbStudent.AutoSize = true;
+            this.rbStudent.Checked = true;
+            this.rbStudent.Location = new System.Drawing.Point(15, 25);
+            this.rbStudent.Name = "rbStudent";
+            this.rbStudent.Size = new System.Drawing.Size(62, 17);
+            this.rbStudent.TabIndex = 0;
+            this.rbStudent.TabStop = true;
+            this.rbStudent.Text = "Student";
+            this.rbStudent.UseVisualStyleBackColor = true;
+            // 
+            // rbProfessional
+            // 
+            this.rbProfessional.AutoSize = true;
+            this.rbProfessional.Location = new System.Drawing.Point(120, 25);
+            this.rbProfessional.Name = "rbProfessional";
+            this.rbProfessional.Size = new System.Drawing.Size(83, 17);
+            this.rbProfessional.TabIndex = 1;
+            this.rbProfessional.Text = "Professional";
+            this.rbProfessional.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(20, 280);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(90, 30);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add to List";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(120, 280);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(90, 30);
+            this.btnRemove.TabIndex = 12;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(220, 280);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(90, 30);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnShowDialog
+            // 
+            this.btnShowDialog.Location = new System.Drawing.Point(20, 320);
+            this.btnShowDialog.Name = "btnShowDialog";
+            this.btnShowDialog.Size = new System.Drawing.Size(190, 30);
+            this.btnShowDialog.TabIndex = 14;
+            this.btnShowDialog.Text = "Show Custom Dialog";
+            this.btnShowDialog.UseVisualStyleBackColor = true;
+            this.btnShowDialog.Click += new System.EventHandler(this.btnShowDialog_Click);
+            // 
+            // lstItems
+            // 
+            this.lstItems.FormattingEnabled = true;
+            this.lstItems.Location = new System.Drawing.Point(350, 40);
+            this.lstItems.Name = "lstItems";
+            this.lstItems.Size = new System.Drawing.Size(250, 160);
+            this.lstItems.TabIndex = 15;
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(350, 210);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.Size = new System.Drawing.Size(520, 200);
+            this.dgvData.TabIndex = 16;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(20, 370);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(280, 23);
+            this.progressBar.TabIndex = 17;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(900, 22);
+            this.statusStrip.TabIndex = 18;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel.Text = "Ready";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 450);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.lstItems);
+            this.Controls.Add(this.btnShowDialog);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.grpStatus);
+            this.Controls.Add(this.chkSubscribe);
+            this.Controls.Add(this.dtpBirthDate);
+            this.Controls.Add(this.lblBirthDate);
+            this.Controls.Add(this.cmbGender);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Complete Windows Forms Demo";
+            this.grpStatus.ResumeLayout(false);
+            this.grpStatus.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_basic_dotnet_forms
+{
+    public partial class MainForm : Form
+    {
+        private List<Person> people = new List<Person>();
+        private Timer progressTimer;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+        }
+
+        private void InitializeCustomComponents()
+        {
+            // Initialize ComboBox
+            cmbGender.Items.AddRange(new string[] { "Male", "Female", "Other" });
+            cmbGender.SelectedIndex = 0;
+
+            // Initialize DataGridView
+            InitializeDataGridView();
+
+            // Initialize Timer for Progress Bar
+            progressTimer = new Timer();
+            progressTimer.Interval = 100;
+            progressTimer.Tick += ProgressTimer_Tick;
+
+            // Set default date
+            dtpBirthDate.Value = DateTime.Now.AddYears(-20);
+        }
+
+        private void InitializeDataGridView()
+        {
+            dgvData.AutoGenerateColumns = false;
+            
+            dgvData.Columns.Add(new DataGridViewTextBoxColumn 
+            { 
+                DataPropertyName = "Name", 
+                HeaderText = "Name", 
+                Width = 150 
+            });
+            
+            dgvData.Columns.Add(new DataGridViewTextBoxColumn 
+            { 
+                DataPropertyName = "Email", 
+                HeaderText = "Email", 
+                Width = 180 
+            });
+            
+            dgvData.Columns.Add(new DataGridViewTextBoxColumn 
+            { 
+                DataPropertyName = "Gender", 
+                HeaderText = "Gender", 
+                Width = 80 
+            });
+            
+            dgvData.Columns.Add(new DataGridViewTextBoxColumn 
+            { 
+                DataPropertyName = "Age", 
+                HeaderText = "Age", 
+                Width = 50 
+            });
+            
+            dgvData.Columns.Add(new DataGridViewCheckBoxColumn 
+            { 
+                DataPropertyName = "IsSubscribed", 
+                HeaderText = "Subscribed", 
+                Width = 80 
+            });
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Validation
+            if (!ValidateInput())
+                return;
+
+            // Create new person
+            var person = new Person
+            {
+                Name = txtName.Text.Trim(),
+                Email = txtEmail.Text.Trim(),
+                Gender = cmbGender.SelectedItem.ToString(),
+                BirthDate = dtpBirthDate.Value,
+                IsSubscribed = chkSubscribe.Checked,
+                Status = rbStudent.Checked ? "Student" : "Professional"
+            };
+
+            // Add to list
+            people.Add(person);
+            
+            // Update ListBox
+            lstItems.Items.Add($"{person.Name} - {person.Email}");
+            
+            // Update DataGridView
+            RefreshDataGridView();
+            
+            // Clear form
+            ClearForm();
+            
+            // Update progress bar
+            UpdateProgressBar();
+            
+            // Update status
+            toolStripStatusLabel.Text = $"Added: {person.Name} | Total: {people.Count}";
+        }
+
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                MessageBox.Show("Please enter a name.", "Validation Error", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtName.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtEmail.Text) || !IsValidEmail(txtEmail.Text))
+            {
+                MessageBox.Show("Please enter a valid email.", "Validation Error", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtEmail.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstItems.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select an item to remove.", "Information", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            int index = lstItems.SelectedIndex;
+            
+            DialogResult result = MessageBox.Show(
+                $"Are you sure you want to remove '{people[index].Name}'?",
+                "Confirm Delete",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                people.RemoveAt(index);
+                lstItems.Items.RemoveAt(index);
+                RefreshDataGridView();
+                UpdateProgressBar();
+                toolStripStatusLabel.Text = $"Item removed | Total: {people.Count}";
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (people.Count == 0)
+            {
+                MessageBox.Show("No data to clear.", "Information", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            DialogResult result = MessageBox.Show(
+                "Are you sure you want to clear all data?",
+                "Confirm Clear",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                people.Clear();
+                lstItems.Items.Clear();
+                RefreshDataGridView();
+                progressBar.Value = 0;
+                toolStripStatusLabel.Text = "All data cleared";
+            }
+        }
+
+        private void btnShowDialog_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new CustomDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    MessageBox.Show($"You entered: {dialog.UserInput}", "Dialog Result", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void ClearForm()
+        {
+            txtName.Clear();
+            txtEmail.Clear();
+            cmbGender.SelectedIndex = 0;
+            dtpBirthDate.Value = DateTime.Now.AddYears(-20);
+            chkSubscribe.Checked = false;
+            rbStudent.Checked = true;
+            txtName.Focus();
+        }
+
+        private void RefreshDataGridView()
+        {
+            dgvData.DataSource = null;
+            dgvData.DataSource = people.ToList();
+        }
+
+        private void UpdateProgressBar()
+        {
+            progressBar.Maximum = 100;
+            progressBar.Value = Math.Min(people.Count * 10, 100);
+            
+            if (!progressTimer.Enabled && progressBar.Value > 0)
+            {
+                progressTimer.Start();
+            }
+        }
+
+        private void ProgressTimer_Tick(object sender, EventArgs e)
+        {
+            // Animate progress bar
+            if (progressBar.Value < progressBar.Maximum)
+            {
+                progressBar.Value += 1;
+            }
+            else
+            {
+                progressTimer.Stop();
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+            toolStripStatusLabel.Text = "New form ready";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Are you sure you want to exit?",
+                "Confirm Exit",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+
+    // Person Model Class
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool IsSubscribed { get; set; }
+        public string Status { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                var today = DateTime.Today;
+                var age = today.Year - BirthDate.Year;
+                if (BirthDate.Date > today.AddYears(-age)) age--;
+                return age;
+            }
+        }
+    }
+}
